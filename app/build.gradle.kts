6dfa1da533ca261ad0f2d8c0ebf71c97a27ee026
afaqkhan707 <106137285+afaqkhan707@users.noreply.github.com>
2026-04-21 01:45:52 +0500

    Migrate deprecated kotlinOptions to compilerOptions (#201)
    Co-authored-by: Don Turner <dturner@users.noreply.github.com>
    Co-authored-by: Ben Sagmoe <bsagmoe@google.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
@@ -21,6 +23,13 @@ plugins {
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
     id("kotlin-parcelize")
+}
+
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_11
+    }
 }
 
 android {
@@ -50,6 +59,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     buildFeatures {
         compose = true
     }
